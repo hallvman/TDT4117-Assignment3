b2440d853e07f7362a509afa2d64e6b98231ea8b
hallvman <Hallv-a@otmail.bo>
2021-10-14 11:32:58 +0200

    Enkelt å lese

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ translator = str.maketrans(
 # 1. Data loading and preprocessing
 texts = f.read().split('\r\n\r\n')
 
-print("Text:", len(texts))
+#print("Text:", len(texts))
 
 paragraphs = []
 for text in texts:
@@ -29,17 +29,14 @@ for text in texts:
         if "gutenberg" not in text.lower():
             paragraphs.append(text)
 
-#print("Paragraphs")
-print("Paragraphs:", paragraphs[3])
+#print("Paragraphs:", paragraphs)
 
-# 1.4 tokenize paragraphs and remove punctuation
+# 1.4 tokenize paragraphs and 1.5 remove punctuation
 words = [None for i in range(len(paragraphs))]
 for i in range(len(paragraphs)):
     words[i] = paragraphs[i].lower().translate(translator).split()
 
-
-#print("Processed_words")
-print("Processed_words:", words[3])
+#print("Processed_words:", words)
 
 # 1.6  Using portstemmer stem words
 stemmed_words = []
@@ -47,8 +44,8 @@ for i in range(len(words)):
     words[i] = [stemmer.stem(word) for word in words[i]]
 
 
-#print("Stemmed_words")
-print("Stemmed_words:", words[3])
+#print("Stemmed_words:", words)
+
 
 
 
