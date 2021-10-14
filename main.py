e8641695f8e4932657e19eafce768761473cf3dd
hallvman <Hallv-a@otmail.bo>
2021-10-14 11:22:10 +0200

    Oppgave 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # 1.0
-import random; random.seed(123)
+import random
+random.seed(123)
 # 1.1
 import codecs
 # 1.5
@@ -7,7 +8,6 @@ import string
 # 1.6
 from nltk.stem.porter import PorterStemmer
 import gensim
-
 
 f = codecs.open("pg3300.txt", "r", "utf-8")
 stemmer = PorterStemmer()
@@ -18,7 +18,9 @@ translator = str.maketrans(
 
 
 # 1. Data loading and preprocessing
-texts = f.read().split('\n\n')
+texts = f.read().split('\r\n\r\n')
+
+print("Text:", len(texts))
 
 paragraphs = []
 for text in texts:
@@ -27,37 +29,26 @@ for text in texts:
         if "gutenberg" not in text.lower():
             paragraphs.append(text)
 
-print("Paragraphs")
-#print("Paragraphs", paragraphs)
+#print("Paragraphs")
+print("Paragraphs:", paragraphs[3])
 
-# 1.4 tokenize paragraphs
+# 1.4 tokenize paragraphs and remove punctuation
 words = [None for i in range(len(paragraphs))]
-for i, p in enumerate(paragraphs):
-    paragraphs[i] = p.split(" ")
-    words[i] = paragraphs[i]
+for i in range(len(paragraphs)):
+    words[i] = paragraphs[i].lower().translate(translator).split()
 
-print("Words")
-#print("Words", words)
 
-# 1.5 Remove punctuation from text
-processed_words = []
-for w in words:
-    for i, word in enumerate(w):
-        # Remove punctuations
-        words[i] = word.lower().translate(translator)
-        if p in words[i]:
-            processed_words[i] = words[i]
-
-print("Processed_words")
-#print("Processed_words", processed_words)
+#print("Processed_words")
+print("Processed_words:", words[3])
 
 # 1.6  Using portstemmer stem words
 stemmed_words = []
-for i, word in enumerate(processed_words):
-    processed_words[i] = stemmer.stem(word)
-    stemmed_words[i] = processed_words[i]
+for i in range(len(words)):
+    words[i] = [stemmer.stem(word) for word in words[i]]
 
-print(stemmed_words)
+
+#print("Stemmed_words")
+print("Stemmed_words:", words[3])
 
 
 
